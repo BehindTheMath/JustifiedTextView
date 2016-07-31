b1b3c6904da9d4690f59c32e1cca40d32edc95d3
Behind The Math <info@behindthemath.io>
2016-07-30 23:29:41 -0400

    Added `pixelsToDimension()`. Added Javadocs.

--- justifiedtextview/src/main/java/io/behindthemath/justifiedtextview/DimensionConverter.java
+++ justifiedtextview/src/main/java/io/behindthemath/justifiedtextview/DimensionConverter.java
@@ -33,29 +33,78 @@ public class DimensionConverter {
     }
     // -- Initialize pattern for dimension string.
     private static final Pattern DIMENSION_PATTERN = Pattern.compile("^\\s*(\\d+(\\.\\d+)?)\\s*([a-zA-Z]+)\\s*$");
-    /*
-     * Based on: https://stackoverflow.com/questions/8343971/how-to-parse-a-dimension-string-and-convert-it-to-a-dimension-value#comment40812977_11353603
+/*
+    //Based on: https://stackoverflow.com/questions/8343971/how-to-parse-a-dimension-string-and-convert-it-to-a-dimension-value#comment40812977_11353603
     private static final Pattern DIMENSION_PATTERN = Pattern.compile("^\\-?\\s*(\\d+(\\.\\d+)?)\\s*([a-zA-Z]+)\\s*$");
-    */
+*/
 
-    public static int stringToDimensionPixelSize(String dimension, DisplayMetrics metrics) {
-        // -- Mimics TypedValue.complexToDimensionPixelSize(int data, DisplayMetrics metrics).
-        InternalDimension internalDimension = stringToInternalDimension(dimension);
-        final float value = internalDimension.value;
-        final float f = TypedValue.applyDimension(internalDimension.unit, value, metrics);
-        final int res = (int)(f + 0.5f);
-        if (res != 0) return res;
-        if (value == 0) return 0;
-        if (value > 0) return 1;
-        return -1;
+    /**
+     * Converts a value in pixels to a value in another unit.
+     *
+     * @param value Value in pixels.
+     * @param unit Complex value type. Must be one of {@link TypedValue#TYPE_DIMENSION}.
+     * @param metrics Current display metrics to use in the conversion.
+     *
+     * @return A value in the unit type specified.
+     */
+    public static float pixelsToDimension(float value, int unit, DisplayMetrics metrics){
+        switch (unit) {
+            case TypedValue.COMPLEX_UNIT_PX:
+                return value;
+            case TypedValue.COMPLEX_UNIT_DIP:
+                return value / metrics.density;
+            case TypedValue.COMPLEX_UNIT_SP:
+                return value / metrics.scaledDensity;
+            case TypedValue.COMPLEX_UNIT_PT:
+                return value / metrics.xdpi * (1.0f/72);
+            case TypedValue.COMPLEX_UNIT_IN:
+                return value / metrics.xdpi;
+            case TypedValue.COMPLEX_UNIT_MM:
+                return value / metrics.xdpi * (1.0f/25.4f);
+        }
+        return 0;
     }
 
+    /**
+     * Mimics TypedValue.complexToDimensionPixelSize(int data, DisplayMetrics metrics).
+     */
+    public static int stringToDimensionPixelSize(String dimension, DisplayMetrics metrics) {
+        InternalDimension internalDimension = stringToInternalDimension(dimension);
+        final float value = internalDimension.value;
+
+        if (value == 0) {
+            return 0;
+        } else if (value > 0) {
+            return 1;
+        } else {
+            final float f = TypedValue.applyDimension(internalDimension.unit, value, metrics);
+            final int res = (int) (f + 0.5f);
+            return res != 0 ? res : -1;
+        }
+    }
+
+    /**
+     * Converts a complex value stored in a String, to a int in the unit specified.
+     * <p>
+     * Mimics {@link TypedValue#complexToDimension TypedValue.complexToDimension(int data, DisplayMetrics metrics)}.
+     *
+     * @param dimension A complex value stored in a String
+     * @param metrics Current display metrics to use in the conversion.
+     *
+     * @return The value stored in an int, in the unit specified.
+     */
     public static float stringToDimension(String dimension, DisplayMetrics metrics) {
-        // -- Mimics TypedValue.complexToDimension(int data, DisplayMetrics metrics).
         InternalDimension internalDimension = stringToInternalDimension(dimension);
         return TypedValue.applyDimension(internalDimension.unit, internalDimension.value, metrics);
     }
 
+    /**
+     * Converts a dimension in the form of a String, to a dimension in the form of a value-unit pair
+     * stored in a {@link InternalDimension InternalDimension} object.
+     *
+     * @param dimension A String containing the dimension.
+     * @return An {@link InternalDimension InternalDimension} containing the dimension.
+     */
     private static InternalDimension stringToInternalDimension(String dimension) {
         // -- Match target against pattern.
         Matcher matcher = DIMENSION_PATTERN.matcher(dimension);
